e4b730bbc1cea9da9f9b9acd0c220f9d34209df0
Constantine Akas <c.akas@alustudent.com>
2026-07-20 15:44:10 -0700

    Implement multiple kernel convolution

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -11,9 +11,9 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     Performs convolution on images using multiple kernels.
 
     Args:
-        images: numpy.ndarray of shape (m,h,w,c)
-        kernels: numpy.ndarray of shape (kh,kw,c,nc)
-        padding: same, valid or tuple
+        images: numpy.ndarray of shape (m, h, w, c)
+        kernels: numpy.ndarray of shape (kh, kw, c, nc)
+        padding: 'same', 'valid', or tuple
         stride: tuple containing strides
 
     Returns:
@@ -25,64 +25,49 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
 
     sh, sw = stride
 
-
     if padding == 'same':
-
         ph = ((h - 1) * sh + kh - h) // 2
         pw = ((w - 1) * sw + kw - w) // 2
 
     elif padding == 'valid':
-
         ph = 0
         pw = 0
 
     else:
-
         ph, pw = padding
-
 
     padded = np.pad(
         images,
-        ((0,0),
-         (ph,ph),
-         (pw,pw),
-         (0,0)),
+        ((0, 0),
+         (ph, ph),
+         (pw, pw),
+         (0, 0)),
         mode='constant'
     )
 
-
-    output_h = ((h + 2*ph - kh)//sh) + 1
-    output_w = ((w + 2*pw - kw)//sw) + 1
-
+    output_h = ((h + 2 * ph - kh) // sh) + 1
+    output_w = ((w + 2 * pw - kw) // sw) + 1
 
     output = np.zeros(
         (m, output_h, output_w, nc)
     )
 
-
     for i in range(output_h):
-
         for j in range(output_w):
-
             row = i * sh
             col = j * sw
 
-
             image_slice = padded[
                 :,
-                row:row+kh,
-                col:col+kw,
+                row:row + kh,
+                col:col + kw,
                 :
             ]
 
-
             for k in range(nc):
-
                 output[:, i, j, k] = np.sum(
                     image_slice * kernels[:, :, :, k],
-                    axis=(1,2,3)
+                    axis=(1, 2, 3)
                 )
 
-
     return output
-
