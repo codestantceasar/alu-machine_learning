398490966ec5449085764892c93bbc34b1c0995c
Constantine Akas <c.akas@alustudent.com>
2026-07-23 11:28:07 -0700

    Implement multiple kernel convolution

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,11 +6,10 @@ This module contains the summation_i_squared function.
 
 def summation_i_squared(n):
     """
-    Calculates the sum of i^2 from i=1 to n without using any loops.
+    Calculates the sum of i^2 from i=1 to n.
     """
     if not isinstance(n, int) or n < 1:
         return None
 
-    # Apply the mathematical formula for the sum of squares
     total_sum = (n * (n + 1) * (2 * n + 1)) // 6
     return total_sum
