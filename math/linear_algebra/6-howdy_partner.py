0824a851cc2ec2f8d7498cd16abce3eaa22cf234
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:09:36 -0700

    Implement multiple kernel convolution

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains the cat_arrays function.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays and returns a new list.
+    """
+    return arr1 + arr2
