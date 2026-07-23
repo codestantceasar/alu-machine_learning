f376fcc1c9c91fc0965d2fe45b3599ebbea7dd33
Constantine Akas <c.akas@alustudent.com>
2026-07-23 09:46:06 -0700

    Implement multiple kernel convolution

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -28,3 +28,37 @@ class MultiNormal:
             centered,
             centered.T
         ) / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a given data point"""
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError(
+                "x must be a numpy.ndarray"
+            )
+
+        d = self.mean.shape[0]
+
+        if x.shape != (d, 1):
+            raise ValueError(
+                "x must have the shape ({}, 1)".format(d)
+            )
+
+        diff = x - self.mean
+
+        determinant = np.linalg.det(self.cov)
+
+        inverse = np.linalg.inv(self.cov)
+
+        exponent = -0.5 * np.dot(
+            np.dot(diff.T, inverse),
+            diff
+        )
+
+        denominator = np.sqrt(
+            ((2 * np.pi) ** d) * determinant
+        )
+
+        pdf_value = np.exp(exponent) / denominator
+
+        return pdf_value.item()
