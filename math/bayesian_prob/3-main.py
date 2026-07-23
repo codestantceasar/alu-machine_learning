0dad848f04370b096fc983b4144d90afb6b88c75
Constantine Akas <c.akas@alustudent.com>
2026-07-23 09:29:05 -0700

    Implement multiple kernel convolution

---
+++ math/bayesian_prob/3-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+posterior = __import__('3-posterior').posterior
+
+P = np.linspace(0, 1, 11)
+
+Pr = np.ones(11) / 11
+
+print(posterior(26, 130, P, Pr))
