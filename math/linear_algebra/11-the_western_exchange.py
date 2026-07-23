c99499bc4641b7f231af3eaeb0060d117c4e8fa1
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:35:51 -0700

    Implement multiple kernel convolution

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Transposes a matrix using NumPy"""
+
+
+def np_transpose(matrix):
+    """Returns a new numpy.ndarray that is the transpose of the matrix"""
+    return matrix.transpose()
