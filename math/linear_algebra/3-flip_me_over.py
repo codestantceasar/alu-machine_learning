e8d778d603eaed4e7f177b95f5b322c567657ef9
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:01:45 -0700

    Implement multiple kernel convolution

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -9,7 +9,6 @@ def matrix_transpose(matrix):
     Returns the transpose of a 2D matrix.
     """
     # Number of columns in the original matrix determines the new row count
-    cols = len(matrix[0])
-    
+    cols = len(matrix[0])    
     # Construct the transposed matrix
     return [[row[i] for row in matrix] for i in range(cols)]
