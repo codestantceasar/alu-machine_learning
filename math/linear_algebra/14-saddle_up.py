46e211a4b6cfe1863a58a6126f9d034948a20910
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:40:08 -0700

    Implement multiple kernel convolution

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Performs matrix multiplication using NumPy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns a new numpy.ndarray that is the product of mat1 and mat2"""
+    return np.matmul(mat1, mat2)
