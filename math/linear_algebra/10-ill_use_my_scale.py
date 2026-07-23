b02d6d07b0f9932e62a3b858baae2802922d5839
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:33:53 -0700

    Implement multiple kernel convolution

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray as a tuple of integers"""
+    return matrix.shape
