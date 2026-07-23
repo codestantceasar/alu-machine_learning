d609e70c1923bed79db38c8c73180b65f3054e85
Constantine Akas <c.akas@alustudent.com>
2026-07-23 12:58:56 -0700

    Implement multiple kernel convolution

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -4,23 +4,20 @@
 
 def determinant(matrix):
     """Computes the determinant of a square matrix"""
-    if len(matrix) == 1 and len(matrix) == 0:
-        return 1
-
     n = len(matrix)
 
     if n == 1:
-        return matrix
+        return matrix[0][0]
 
     if n == 2:
-        return (matrix * matrix -
-                matrix * matrix)
+        return (matrix[0][0] * matrix[1][1] -
+                matrix[0][1] * matrix[1][0])
 
     det = 0
     for c in range(n):
         sub_matrix = [row[:c] + row[c + 1:] for row in matrix[1:]]
         sign = 1 if c % 2 == 0 else -1
-        det += sign * matrix[c] * determinant(sub_matrix)
+        det += sign * matrix[0][c] * determinant(sub_matrix)
     return det
 
 
@@ -37,9 +34,9 @@ def adjugate(matrix):
         if len(row) != n or n == 0 or len(row) == 0:
             raise ValueError("matrix must be a non-empty square matrix")
 
-    # The adjugate of a 1x1 matrix is defined as [] by convention
+    # The adjugate of a 1x1 matrix [[x]] is always [[1]]
     if n == 1:
-        return []
+        return [[1]]
 
     # 1. Calculate the Cofactor Matrix
     cofactor_matrix = []
